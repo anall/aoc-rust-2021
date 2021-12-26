14d5212850f757b607b08c4ba12e341ab0960a31
Andrea Nall <anall@andreanall.com>
2021-12-26 17:48:30 -0600

    cleanup, return only count

--- src/day12.rs
+++ src/day12.rs
@@ -6,27 +6,20 @@ use regex::Regex;
 
 fn get_neighbors<'a>((node,twice,seen) : &(&'a str, Option<&'a str>, usize), edges : &'a HashMap<String,(usize,bool,HashSet<String>)>) -> Vec<(&'a str,Option<&'a str>,usize)> {
     let (_,_,neighbors) = edges.get(*node).unwrap();
-    if *node == "end" { // end can never be left
-        vec![]
-    } else {
-        neighbors.iter().filter_map(|neighbor| {
-            let (neighbor_flag,is_uppercase,_) = edges.get(neighbor.as_str()).unwrap();
-            if *is_uppercase { // uppercase nodes can be visited as many times as we'd like
-                Some( (neighbor.as_str(),*twice,seen | neighbor_flag) )
-            } else if *neighbor == "start" { // Start can never be reentered
-                None
-            } else if seen & neighbor_flag == 0 { // visiting for first time
-                Some( (neighbor.as_str(),*twice,seen | neighbor_flag) )
-            } else if twice.is_none() { // we're can visit this one twice
-                assert!(seen & neighbor_flag != 0);
-                Some( (neighbor.as_str(),Some(neighbor.as_str()),*seen) )
-            } else {
-                None
-            }
-        }).collect::<Vec<(&'a str,Option<&'a str>,usize)>>()
-    }
+    neighbors.iter().filter_map(|neighbor| {
+        let (neighbor_flag,is_uppercase,_) = edges.get(neighbor.as_str()).unwrap();
+        if *is_uppercase || seen & neighbor_flag == 0 { // uppercase nodes can be visited as many times as we'd like
+            Some( (neighbor.as_str(),*twice,seen | neighbor_flag) )
+        } else if twice.is_none() { // we're can visit this one twice
+            assert!(seen & neighbor_flag != 0);
+            Some( (neighbor.as_str(),Some(neighbor.as_str()),*seen) )
+        } else {
+            None
+        }
+    }).collect::<Vec<(&'a str,Option<&'a str>,usize)>>()
 }
 
+#[allow(unused)]
 fn walk_and_find_internal<'a>(cur : (&'a str,Option<&'a str>,usize), path : &[&'a str], edges : &'a HashMap<String,(usize,bool,HashSet<String>)>, out : &mut Vec<Vec<&'a str>>) {
     let mut new_path = path.to_vec();
     new_path.push(cur.0);
@@ -42,12 +35,36 @@ fn walk_and_find_internal<'a>(cur : (&'a str,Option<&'a str>,usize), path : &[&'
     }
 }
 
+#[allow(unused)]
 fn walk_and_find(allow_double_visit : bool, edges : &HashMap<String,(usize,bool,HashSet<String>)>) -> Vec<Vec<&str>> {
     let mut out = Vec::new();
     let start_flag = edges.get("start").unwrap().0;
     walk_and_find_internal( 
         ("start", if allow_double_visit { None } else { Some("PLACEHOLDER") }, start_flag), 
         &[], edges, &mut out);
+    out
+}
+
+
+
+fn walk_and_count_internal<'a>(cur : (&'a str,Option<&'a str>,usize), edges : &'a HashMap<String,(usize,bool,HashSet<String>)>, out : &mut usize) {
+    if cur.0 == "end" {
+        *out += 1;
+    } else {
+        let neighbors = get_neighbors(&cur, edges);
+
+        for neighbor in neighbors {
+            walk_and_count_internal(neighbor, edges, out);
+        }
+    }
+}
+
+fn walk_and_count(allow_double_visit : bool, edges : &HashMap<String,(usize,bool,HashSet<String>)>) -> usize {
+    let mut out = 0;
+    let start_flag = edges.get("start").unwrap().0;
+    walk_and_count_internal( 
+        ("start", if allow_double_visit { None } else { Some("PLACEHOLDER") }, start_flag), 
+        edges, &mut out);
     out
 }
 
@@ -65,25 +82,29 @@ fn main() -> aoc::Result<()> {
             let left = &cap[1];
             let right = &cap[2];
 
-            out.entry(left.to_string()).or_insert_with(|| {
-                let cbit = bit;
-                bit <<= 1;
-                (cbit,left == left.to_ascii_uppercase(),HashSet::new()) }).2.insert(right.to_string());
-            out.entry(right.to_string()).or_insert_with(|| {
-                let cbit = bit;
-                bit <<= 1;
-                (cbit,right == right.to_ascii_uppercase(),HashSet::new()) }).2.insert(left.to_string());
+            // start cannot be reentered
+            if right != "start" {
+                out.entry(left.to_string()).or_insert_with(|| {
+                    let cbit = bit;
+                    bit <<= 1;
+                    (cbit,left == left.to_ascii_uppercase(),HashSet::new()) }).2.insert(right.to_string());
+            }
+            if left != "start" {
+                out.entry(right.to_string()).or_insert_with(|| {
+                    let cbit = bit;
+                    bit <<= 1;
+                    (cbit,right == right.to_ascii_uppercase(),HashSet::new()) }).2.insert(left.to_string());
+            }
         }
+
+        // end cannot be exited once entered
+        out.get_mut("end").unwrap().2.clear();
+        
         out
     };
 
-    let result_once = walk_and_find(false, &edges);
-
-    println!("{:?}",result_once.len());
-
-    let result_twice = walk_and_find(true, &edges);
-
-    println!("{:?}",result_twice.len());
+    println!("{}",walk_and_count(false, &edges));
+    println!("{}",walk_and_count(true, &edges));
 
     Ok( () )
 }
